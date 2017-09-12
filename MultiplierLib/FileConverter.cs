a02f0109ce46156ec24a8dd50b2927cd23e623e9
JessiRoa <jessicaroar90@hotmail.com>
2017-09-12 18:11:51 +0200

    Unit test: PurchaseCalculationTests och FileConversionTests har något fel.

---
+++ MultiplierLib/FileConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplierLib
+{
+    class FileConverter
+    {
+        public string Transform(string row)
+        {
+            string[] tokens = row.Split(';');
+            var firstName = tokens[0].ToUpper();
+            var lastName = tokens[1].ToUpper();
+            var years = DateTime.Now.Year - int.Parse(tokens[2]);
+            var message = $"{firstName} {lastName} ÄR {years} ÅR GAMMAL.";
+            return message;
+        }
+
+        public void TransformFile(string inputName, string outputName)
+        {
+            //StringWriter sw = new StringWriter();
+            //string[] lines = FileConverter.ReadAllLines(inputName);
+            //foreach (var line in lines)
+            //{
+            //    sw.WriteLine(Transform(line));
+            //}
+            //sw.Close();
+            //File.WriteAllText(outputName, sw.toString());
+
+        }
+    }
+}
---
+++ MultiplierTestsLib/FileConversionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultiplierTestsLib
+{
+    [TestClass]
+    public class FileConversionTests
+    {
+        [TestMethod]
+        public void jessica_roa_1990()
+        {
+            var sut = new FileConverter();
+            string actual = sut.Transform("marco;erice;1978");
+            Assert.AreEqual("MARCO ERICE ÄR 39 ÅR GAMMAL.", actual);
+        }
+        [TestMethod]
+        public void mariela_gomez_1930()
+        {
+            var sut = new FileConverter();
+            string actual = sut.Transform("annai;erice;2016");
+            Assert.AreEqual("ANNAI ERICE ÄR 1 ÅR GAMMAL.", actual);
+        }
+    }
+}
