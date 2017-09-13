2cac6e1bbfa950c3c7952d5d3b2f7c260a6e0b5e
JessiRoa <jessicaroar90@hotmail.com>
2017-09-13 11:24:17 +0200

    Uppgift Lek_2

---
+++ MultiplierLib/ListHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplierLib
+{
+    public class ListHandler
+    {
+        public List<string> List = new List<string>();
+
+        public void AddFiveStrings()
+        {
+            List.Add("1");
+            List.Add("2");
+            List.Add("3");
+            List.Add("4");
+            List.Add("5");
+        }
+        /*for (i=1; i <= 6; i++)
+         List add("test" +  i);*/
+
+        public void RemoveThirdString()
+        {
+            List.Remove("2");
+        }
+
+        public void Sort()
+        {
+            //Console.WriteLine("{2}", List);
+            List.Sort();
+        }
+    }
+}
