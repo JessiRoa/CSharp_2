87815fc7f05bcaf08cca965630755c3667b978b7
JessiRoa <jessicaroar90@hotmail.com>
2017-09-18 02:08:54 +0200

    Koden har 4 failed test på TellStoryNames

---
+++ StoretellerTestsLib/StoretellerTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+//using System.Text;
+//using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StorytellerLib;
+
+namespace StoretellerTestsLib
+{
+    [TestClass]
+    public class StoretellerTests
+    {
+        [TestMethod]
+        public void TellStoryToYoungNotScary()
+        {
+            // Arrange
+            var sut = new Storyteller();
+            // Act
+            var actual = sut.TellStory("Jessica", 27, false);
+            // Assert
+            Assert.AreEqual("Lyssna nu Jessica. Det var en gång en prinsessa " + "som tyckte om att dansa", actual);
+        }
+        [TestMethod]
+        public void TellStoryToOldYoungJessicaNotScary()
+        {
+            // Arrange
+            var sut = new Storyteller();
+            // Act
+            string actual = sut.TellStory("Jessica", 27, true);
+            Assert.AreEqual($"Lyssna nu Jessica.  Det var en gång en prinsessa " + "som egentligen var en varulv", actual);
+        }
+        [TestMethod]
+        public void TellStoryToOldJohnnyNotScary()
+        {
+            // Arrange
+            var sut = new Storyteller();
+            // Act
+            string actual = sut.TellStory("Johhny", 32, false);
+            Assert.AreEqual($"Hallå Johnny! En gång fanns en kille som tyckte om att dansa.", actual);
+        }
+        [TestMethod]
+        public void TellStoryToOldJohnnyScary()
+        {
+            // Arrange
+            var sut = new Storyteller();
+            // Act
+            string actual = sut.TellStory("Johnny", 32, true);
+            Assert.AreEqual($"Hallå Johnny! En gång fanns en kille som egentligen var en varulv.", actual);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TellStoryToYoungScary()
+        {
+            // Arrange
+            var sut = new Storyteller();
+            // Act
+            string actual = sut.TellStory("Mariela", 4, true);
+            // Assert
+            // Assert.AreEqual("Hallå Marco! En gång fanns en kille " + "som egentligen var en varulv", actual);
+        }
+    }
+}
+    
---
+++ StorytellerLib/Storyteller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorytellerLib
+{
+    public class Storyteller
+    {
+        private string myStory;
+
+        public string TellStory(string nameOfChild, int ageOfChild, bool willBeScary)
+        {
+            if (ageOfChild < 5 && willBeScary)
+                throw new InvalidOperationException();
+                if (ageOfChild < 7)
+            {
+                string myStory = $"Lyssna nu { nameOfChild}. Det var en gång en prinsessa";
+            }
+        else
+        myStory = $"Hallå {nameOfChild}! En gång fanns en kille ";
+             
+            string scary = "";
+            if (willBeScary == true)
+            {
+                scary = $"som egentligen var en varulv";
+            }
+            else
+                scary = $"som tyckte om att dansa";
+            
+            return myStory + scary;
+        }
+    }
+}
